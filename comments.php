615c8e12ea8b04ab39f20de6550af2eb7339c3d1
Stefan <sk@kanen.at>
2012-03-06 21:39:50 +0100

    Use Newsgrape Comment System for articles that aren't synced with newsgrape.com. closes #115

--- comments.php
+++ comments.php
@@ -10,12 +10,17 @@ $ngcp_provider = "http://staging.newsgrape.com";
 <div id="ng_excomments"></div>
 
 <script type="text/javascript">
-    ng_article_id = <?php echo $ngcp_id; ?>;
-    ng_provider = "<?php echo $ngcp_provider; ?>";
-    (function(d, t) {
-        var ng  = d.createElement(t),
-        s  = d.getElementsByTagName(t)[0];
-        ng.async = ng.src = '<?php echo $ngcp_provider; ?>/static/js/excomments_consumer.js';
-        s.parentNode.insertBefore(ng, s);
-    }(document, 'script'));
+<?php if ("" != $ngcp_id): ?>
+	ng_article_id = <?php echo $ngcp_id; ?>;
+<?php else: ?>
+	ng_site_id = "<?php echo get_option('ngcp_blog_id'); ?>";
+	ng_post_id = "<?php echo $id; ?>";
+<?php endif; ?>
+	ng_provider = "<?php echo $ngcp_provider; ?>";
+	(function(d, t) {
+		var ng  = d.createElement(t),
+		s  = d.getElementsByTagName(t)[0];
+		ng.async = ng.src = '<?php echo $ngcp_provider; ?>/static/js/excomments_consumer.js';
+		s.parentNode.insertBefore(ng, s);
+	}(document, 'script'));
 </script>
