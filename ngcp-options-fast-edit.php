6555e9686a220273f82f916a6d76f59596db5340
Stefan Kröner <sk@Stefans-MacBuch-Luft.local>
2012-08-21 12:56:02 +0200

    make progress bar work in IE (progress-polyfill)

--- ngcp-options-fast-edit.php
+++ ngcp-options-fast-edit.php
@@ -7,20 +7,20 @@
 	$msg = array();
 	$msgtype = 'error';
 	$api = new NGCP_API();
-	
+
 	$options = ngcp_get_options();
 
 	$updated_articles = array();
-	
+
 	// sometimes these input fields are not set. until we find out why, do this check:
 	if(!isset($input['sync_hidden']) || !isset($input['type_hidden']) || !isset($input['is_synced_hidden']) || !isset($input['promotional_hidden']) || !isset($input['adult_only_hidden'])) {
 	    add_settings_error( 'ngcp_fe', 'ngcp_fe', __('Newsgrape Fast editing failed'), 'error' );
 	    unset($input);
 	    return $input;
 	}
-	
+
 	$fields = array( 'sync', 'type', 'promotional', 'adult_only');
-	
+
 	foreach ($fields as $field) {
 		foreach ($input[$field.'_hidden'] as $post_id => $old_value) {
 			if ($input[$field][$post_id] != $old_value) {
@@ -30,11 +30,11 @@
 			}
 		}
 	}
-	
+
 	$updated_articles = array_unique($updated_articles);
-	
+
 	$unsynced_articles = array();
-	
+
 	foreach ($input['is_synced_hidden'] as $post_id => $is_synced) {
 		// has not been synced but sync is now checked
 		if (False == $is_synced && 1 == $input['sync'][$post_id]) {
@@ -46,7 +46,7 @@
 			ngcp_debug('unsynced '.$post_id);
 		}
 	}
-	
+
 	$articles_to_sync = array_unique(array_merge($updated_articles, $unsynced_articles));
 	ngcp_debug('Articles to sync: '.sizeof($articles_to_sync));
 
@@ -58,7 +58,7 @@
 
 	// Send custom updated message
 	$msg = implode('<br />', $msg);
-	
+
 	if (empty($msg)) {
 		if (0 == sizeof($updated_articles) && 0 == sizeof($articles_to_sync)) {
 			$msg = __('Nothing has changed.', 'ngcp');
@@ -67,38 +67,49 @@
 		}
 		$msgtype = 'updated';
 	}
-	
+
 	add_settings_error( 'ngcp_fe', 'ngcp_fe', $msg, $msgtype );
-	
+
 	// Nothing to save to options db ...
 	unset($input);
-	
+
 	// ... except 'published_old', so the info box in the options page disappears
 	$options['published_old'] = 1;
 	update_option('ngcp',$options);
-	
+
 	return $input;
 }*/
 
 add_action('wp_ajax_ngcp_sync', 'ngcp_ajax_sync');
+
+wp_register_script('ngcp-progress', ngcp_plugin_dir_url().'progress-polyfill.min.js');
+
+function ngcp_load_custom_wp_admin_scripts() {
+	wp_register_style('ngcp_progress', ngcp_plugin_dir_url().'progress-polyfill.css');
+	wp_register_script('ngcp_progress', ngcp_plugin_dir_url().'progress-polyfill.min.js', null, null, True);
+    wp_enqueue_script('ngcp_progress');
+    wp_enqueue_style('ngcp_progress');
+}
+
+add_action( 'admin_enqueue_scripts', 'ngcp_load_custom_wp_admin_scripts' );
 
 function ngcp_ajax_sync() {
 	$id = $_POST['id'];
 	ngcp_debug("AJAX request - sync $id");
 
 	$fields = array( 'sync', 'type', 'promotional', 'adult_only');
-	
+
 	foreach ($fields as $field) {
 		if (isset($_POST[$field])) {
 			update_post_meta($id, 'ngcp_'.$field, $_POST[$field]);
 			ngcp_debug('ngcp_'.$field.' = '.$_POST[$field]);
 		}
 	}
-	
+
 	$success = NGCP_Core_Controller::edit($id);
-	
+
 	global $ngcp_error;
-	
+
 	// response output
     header( "Content-Type: application/json" );
     $response = json_encode( array(
@@ -107,7 +118,7 @@ function ngcp_ajax_sync() {
 		'id' => $id,
 		'message' => $ngcp_error) );
     echo $response;
-	
+
 	//Important
 	exit;
 }
@@ -120,10 +131,9 @@ function ngcp_add_fe_page() {
 }
 
 // Display the options page
-function ngcp_display_fast_edit() {
-?>
+function ngcp_display_fast_edit() { ?>
 
-<? include_once 'options-head.php';  ?>
+<?php include_once 'options-head.php'; ?>
 
 <script type="text/javascript">
 var post_ids = Array();
@@ -131,7 +141,7 @@ var names = {};
 
 function deletion_check(form) {
 	delete_count = 0;
-	
+
 	jQuery(".ngcp-all-articles tbody tr").each(function(){
 		var old_checked = jQuery(this).find("input[name^='ngcp_fe[sync_hidden]']")[0].value != "";
 		var new_checked = jQuery(this).find("input[name^='ngcp_fe[sync]']")[0].checked;
@@ -139,28 +149,28 @@ function deletion_check(form) {
 			delete_count++;
 		}
 	});
-	
+
 	if(delete_count > 0) {
 		var text = delete_count > 1 ? "<?php _e('Do you really want to unpublish {count} articles? They will be deleted on Newsgrape'); ?>" : "<?php _e('Do you really want to unpublish {count} article? It will be deleted on Newsgrape.'); ?>";
 		return confirm(text.replace('{count}', delete_count));
 	}
-	
+
 	return true;
 }
 </script>
 
 <div class="wrap">
 	<form method="post" id="ngcp_fe" name ="ngcp_fe" action="options.php">
-		<?php 
+		<?php
 		settings_fields('ngcp_fe');
 		//settings_errors('ngcp_fe');
 		$options = ngcp_get_options();
 		$categories = $options['categories'];
 		$posts = query_posts('posts_per_page=-1'); /* Alters Main Loop */
 		?>
-		
+
 		<h2><?php _e('Newsgrape Sync Fast Edit Articles', 'ngcp'); ?></h2>
-		
+
 		<a id="ngcp-help" href="#" class="hide-if-no-js"><?php _e('What is "News-Related", what is "Fiction"?', 'ngcp'); ?></a>
 
 		<div id="ngcp-help-text" class="hide-if-js">
@@ -170,8 +180,8 @@ In school you write "opinion essays" - a "News-Related"-Article is basically the
 <strong>What is "Fiction"?</strong><br />
 A „Fiction“-Article is any text that you just make up in your mind. When writing a "Fiction"-Article you can let your mind wander – it is fictional and you can write about whatever you want. It is usually not related to a certain article or comment. It might be a short story, parody or a poem.', 'ngcp'); ?>
 		</div>
-		
-		
+
+
 		<table class="widefat ngcp-all-articles">
 			<thead>
 				<tr class="ngcp-edit-all">
@@ -186,7 +196,7 @@ A „Fiction“-Article is any text that you just make up in your mind. When wri
 						<select name="ngcp-type-all" id="ngcp-type-all">
 							<option value="" selected="selected"><?php _e('Type','ngcp'); ?></option>
 							<option value="opinion"><?php _e('News-Related','ngcp'); ?></option>
-							<option value="creative"><?php _e('Fiction','ngcp'); ?></option>		
+							<option value="creative"><?php _e('Fiction','ngcp'); ?></option>
 						</select>
 					</th>
 					<th class="promotional">
@@ -203,10 +213,10 @@ A „Fiction“-Article is any text that you just make up in your mind. When wri
 					</th>
 				</tr>
 			</thead>
-			
+
 			<tbody>
 				<?php while ( have_posts() ) : the_post(); ?>
-				
+
 				<?php
 					setup_postdata($post);
 					$post_meta = get_post_custom($post->ID);
@@ -214,16 +224,16 @@ A „Fiction“-Article is any text that you just make up in your mind. When wri
 					$is_synced = array_key_exists('ngcp_id', $post_meta) && $post_meta['ngcp_id'][0] != 0 && (!array_key_exists('ngcp_deleted', $post_meta) || False == $post_meta['ngcp_deleted']);
 					$is_promotional = False == $post_meta['ngcp_promotional'];
 				?>
-				
+
 				<script type="text/javascript">post_ids.push(<?php the_id(); ?>); names["<?php the_id(); ?>"] = "<?php the_title(); ?>";</script>
-				
+
 				<tr class="<?php if($is_synced) echo 'ngcp-synced'; ?> <?php if ($has_type) { echo 'ngcp-has-type'; } else { echo 'ngcp-has-no-type'; } ?>">
 					<input type="hidden" name="ngcp_fe[is_synced_hidden][<?php the_id(); ?>]" value="<?php echo $is_synced; ?>">
 					<input type="hidden" name="ngcp_fe[sync_hidden][<?php the_id(); ?>]" value="<?php echo $post_meta['ngcp_sync'][0]||0; ?>">
 					<input type="hidden" name="ngcp_fe[type_hidden][<?php the_id(); ?>]" value="<?php echo $post_meta['ngcp_type'][0]; ?>">
 					<input type="hidden" name="ngcp_fe[promotional_hidden][<?php the_id(); ?>]" value="<?php echo $post_meta['ngcp_promotional'][0]; ?>">
 					<input type="hidden" name="ngcp_fe[adult_only_hidden][<?php the_id(); ?>]" value="<?php echo $post_meta['ngcp_adult_only'][0]; ?>">
-					
+
 					<td>
 						<a href="<?php the_permalink(); ?>" class="ngcp-the-title"><?php the_title(); ?></a>
 						<?php edit_post_link(); ?>
@@ -245,7 +255,7 @@ A „Fiction“-Article is any text that you just make up in your mind. When wri
 					<td>
 						<select class="ngcp-select-type" name="ngcp_fe[type][<?php the_id(); ?>]">
 							<option value="opinion" <?php selected($post_meta['ngcp_type'][0], 'opinion'); ?>><?php _e('News-Related','ngcp'); ?></option>
-							<option value="creative" <?php selected($post_meta['ngcp_type'][0], 'creative'); ?>><?php _e('Fiction','ngcp'); ?></option>		
+							<option value="creative" <?php selected($post_meta['ngcp_type'][0], 'creative'); ?>><?php _e('Fiction','ngcp'); ?></option>
 						</select>
 					</td>
 					<td>
@@ -280,20 +290,20 @@ A „Fiction“-Article is any text that you just make up in your mind. When wri
 				<?php endwhile; ?>
 			</tbody>
 		</table>
-		
+
 		<p class="submit">
 			<input type="submit" id="ngcp_fe_save" name="ngcp_fe[save]" value="<?php esc_attr_e('Save Changes','ngcp'); ?>" class="button-primary" />
 		</p>
-		
+
 	<script type="text/javascript">
 		jQuery(document).ready(function($){
 			$(function () {
 				$('#ngcp_fe_save').click(function(event) {
 					event.preventDefault();
-					
+
 					if (!deletion_check(ngcp_fe))
 						return;
-						
+
 					var values = {};
 					var sync = Array();
 					$.each($('#ngcp_fe').serializeArray(), function(i, field) {
@@ -312,18 +322,18 @@ A „Fiction“-Article is any text that you just make up in your mind. When wri
 							sync.push(id);
 						}
 					};
-					
+
 					if (sync.length == 0) {
 						alert("Nothing changed");
 					} else {
 						p = $('#ngcp-sync-progress')[0];
 						p.max = sync.length;
 						$('#ngcp-sync-goal').html(sync.length);
-						
+
 						ngcp_overlay('display');
-						
+
 						var has_errors = false;
-						
+
 						for (var i = 0; i <sync.length; i++) {
 							id = sync[i];
 
@@ -353,18 +363,18 @@ A „Fiction“-Article is any text that you just make up in your mind. When wri
 									$('.ngcp-sync-button').html("<?php _e('Close', 'ngcp'); ?>");
 									$('.ngcp-sync-button').attr("onclick","javascript:location.reload(true);");
 									if(has_errors) {
-											
+
 										$('#ngcp-sync-status').html("<?php _e('Finished syncing!', 'ngcp'); ?>");
-									} else { 
+									} else {
 									}
 								}
 							}, 'json');
 						}
-					
+
 
 					}
-					
-					
+
+
 
 				});
 				$('#ngcp-help').click(function(event) {
@@ -398,13 +408,13 @@ A „Fiction“-Article is any text that you just make up in your mind. When wri
 						$('.ngcp-adult').prop("checked", false);
 					}
 				});
-	
+
 			});
 
-			
-			
+
+
 		});
-		
+
 		function ngcp_overlay(mode) {
 				if(mode == 'display') {
 					if(document.getElementById("ngcp-overlay") === null) {
@@ -421,7 +431,7 @@ A „Fiction“-Article is any text that you just make up in your mind. When wri
 					document.getElementsByTagName("body")[0].removeChild(document.getElementById("ngcp-overlay"));
 				}
 			}
-		
+
 	</script>
 </div>
 
@@ -436,7 +446,6 @@ A „Fiction“-Article is any text that you just make up in your mind. When wri
 	<button class="ngcp-sync-button" onclick="ngcp_overlay('none')"><?php _e('Cancel', 'ngcp'); ?></button>
 </div>
 
-		
 <?php
 }
 ?>
