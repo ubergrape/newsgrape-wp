6ece3bd4b956ab7ba871ed0685d8bfa005bf5f73
Stefan Kröner <sk@Stefans-MacBuch-Luft.local>
2012-08-30 17:28:31 +0200

    fix javascript on "New Page" page

--- js/ngcp-metabox.js
+++ js/ngcp-metabox.js
@@ -4,7 +4,7 @@ jQuery(function(){
 		jQuery('#ngcp-license-display').html(jQuery('#ngcp_license option:selected').text());
 		jQuery('#ngcp-type-display').html(jQuery('input[name=ngcp_type]:checked').parent().text());
 	}
-	
+
 	jQuery('#ngcp-language-select').siblings('a.edit-ngcp-language').click(function() {
 		if (jQuery('#ngcp-language-select').is(":hidden")) {
 			jQuery('#ngcp-language-select').slideDown('fast');
@@ -12,7 +12,7 @@ jQuery(function(){
 		}
 		return false;
 	});
-	
+
 	jQuery('.save-ngcp-language', '#ngcp-language-select').click(function() {
 		jQuery('#ngcp-language-select').slideUp('fast');
 		jQuery('#ngcp-language-select').siblings('a.edit-ngcp-language').show();
@@ -27,7 +27,7 @@ jQuery(function(){
 		ngcpUpdateText();
 		return false;
 	});
-	
+
 	jQuery('#ngcp-license-select').siblings('a.edit-ngcp-license').click(function() {
 		if (jQuery('#ngcp-license-select').is(":hidden")) {
 			jQuery('#ngcp-license-select').slideDown('fast');
@@ -50,13 +50,13 @@ jQuery(function(){
 		ngcpUpdateText();
 		return false;
 	});
-	
+
 	jQuery('.cancel-ngcp-type').click(function() {
 		jQuery('#ngcp_type_' + jQuery('#hidden_ngcp_type').val()).prop('checked', true);
 		ngcpUpdateText();
 		return false;
 	});
-	
+
 	jQuery('#ngcp_more').click(function() {
 		jQuery('#ngcp_more_inner').slideDown('fast');
 		jQuery(this).hide();
@@ -69,9 +69,9 @@ jQuery(function(){
 		jQuery('#ngcp_more').show();
 		return false;
 	});
-	
+
 	/* hide/show ngcp_description-prompt-text */
-	
+
 	wptitlehint = function(id) {
 		id = id || 'title';
 
@@ -96,13 +96,17 @@ jQuery(function(){
 		});
 	}
 
-	wptitlehint('ngcp_description');
-	
-	/* Move newsgrape description box above article body editor*/
-	jQuery('#newsgrape_description').appendTo('#titlediv');
-	
-	/* Fix tab indices */
-	jQuery('#titlediv input')[0].tabIndex = 100; // title
-	jQuery('#newsgrape_description_inner input')[0].tabIndex = 101; // newsgrape description
-	jQuery('#postdivrich textarea')[0].tabIndex = 102; // main text
+	/* only run where the newsgrape description is enabled. posts/pages */
+	if(jQuery('#newsgrape_description').length != 0) {
+		wptitlehint('ngcp_description');
+
+		/* Move newsgrape description box above article body editor*/
+		jQuery('#newsgrape_description').appendTo('#titlediv');
+
+		/* Fix tab indices */
+		jQuery('#titlediv input')[0].tabIndex = 100; // title
+		jQuery('#newsgrape_description_inner input')[0].tabIndex = 101; // newsgrape description
+		jQuery('#postdivrich textarea')[0].tabIndex = 102; // main text
+	}
+
 });
